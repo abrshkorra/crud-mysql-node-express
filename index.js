1b417c5d826780eed9d6a21f19cf95e1c3ce9d4a
abrshkorra <abrshkorra@gmail.com>
2023-11-16 10:31:05 +0300

    routes + controller

--- index.js
+++ index.js
@@ -1,8 +1,12 @@
-const express = require('express');
-const db = require('./db');
+const express = require('express'),
+db = require('./db'),
+userRoutes = require('./controllers/user-controller') 
 
 const app = express();
 const port = 3000;
+
+// middleware
+app.use('/api/users', userRoutes)
 
 db.query("SELECT * FROM users")
   .then(data => console.log(data))
